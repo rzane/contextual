72d27cc0c8ab9a0d1ae1570ae8e9d684d11a8855
Ray Zane <ray@promptworks.com>
2018-02-23 11:49:39 -0500

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Contextual.MixProject do
   def project do
     [
       app: :contextual,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.6",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
